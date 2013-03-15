0f793b1017c4a5487030ed8172302f27dbf0b44c
zmejia <zaidamejia@gmial.com>
2013-03-15 14:46:00 -0700

    new changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 
-# 3/5/2013 HW 2 part 2
-#created the variable @all_ratings
+# 3/5/2013 HW 2 part 1b, 2, & 3
+#created the variable @all_ratings, @sort
 #created session
 
 class MoviesController < ApplicationController
@@ -13,10 +13,48 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-    
+    if params[:sort]
+      @sort = params[:sort]
+    end
     @all_ratings = Movie.all_ratings
-    @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
-    session[:param] = params
+    redirect = false
+    logger.debug(session.inspect)
+
+    # hw2 part 2 sorting by ratings
+    #sorting by rating when refresh is clciked
+    if params[:commit] == "Refresh" and params[:ratings].nil?
+      @ratings = nil
+      session[:ratings] = nil
+    elsif params[:ratings]
+      @ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      redirect = true
+    else
+      @ratings = nil
+    end
+
+    if redirect
+      flash.keep
+      redirect_to movies_path :sort=>@sort, :ratings=>@ratings
+    end
+
+     #sorts by raiting
+    if @ratings and @sort
+      @movies = Movie.where(:rating => @ratings.keys).find(:all, :order => (@sort_by))
+    elsif @ratings
+      @movies = Movie.where(:rating => @ratings.keys)
+    elsif @sort
+      @movies = Movie.find(:all, :order => (@sort))
+    else
+      @movies = Movie.all
+    end
+    if !@ratings
+      @ratings = Hash.new
+    end 
+
+   session[:param] = params
   end
 
   def new
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,5 @@
 # Feb. 28, hw2 1.b
 # created sort method 
-# created helper_class method
 
 module MoviesHelper
   # Checks if a number is odd:
@@ -8,19 +7,12 @@ module MoviesHelper
     count.odd? ?  "odd" :  "even"
   end
 
+  #sorts movies by title or release date
   def sort(movie)
   	if(params[:sort].to_s == 'title')
   		return movie.title
   	elsif(params[:sort].to_s == 'release')
-  		return movie.release_date.to_s
-  	end
-  end
-
-  def helper_class(value)
-  	if(params[:sort].to_s == value) 
-  		return 'hilite'
-  	else
-  		return nil
+  		return movie.release_date
   	end
   end
 
